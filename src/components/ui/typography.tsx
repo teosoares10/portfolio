0723f8693faf674ccffb77a7359aca7146cd2555
Teodoro Pedro <teosoares10@gmail.com>
2024-08-02 15:07:41 +0100

    refactor: change content prop to children prop

--- src/components/ui/typography.tsx
+++ src/components/ui/typography.tsx
@@ -1,33 +1,33 @@
 import { cn } from "@/lib/utils";
 
 type TypographyProps = {
-  content: React.ReactNode;
+  children: React.ReactNode;
   className?: string;
   as: "h1" | "h2" | "h3" | "h4" | "p" | "span";
 };
 
 export const Typography = ({
-  content,
+  children,
   className,
   as = "p",
 }: TypographyProps) => {
   switch (as) {
     case "h1":
-      return <h1 className={cn("", className)}>{content}</h1>;
+      return <h1 className={cn("", className)}>{children}</h1>;
 
     case "h2":
-      return <h2 className={cn("", className)}>{content}</h2>;
+      return <h2 className={cn("", className)}>{children}</h2>;
 
     case "h3":
-      return <h3 className={cn("", className)}>{content}</h3>;
+      return <h3 className={cn("", className)}>{children}</h3>;
 
     case "h4":
-      return <h4 className={cn("", className)}>{content}</h4>;
+      return <h4 className={cn("", className)}>{children}</h4>;
 
     case "span":
-      return <span className={cn("", className)}>{content}</span>;
+      return <span className={cn("", className)}>{children}</span>;
 
     default:
-      return <p className={className}>{content}</p>;
+      return <p className={className}>{children}</p>;
   }
 };
