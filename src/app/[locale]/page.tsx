c98ab4cb580e5696ce1bc5f38d879ae9b4de9945
Teodoro Pedro <teosoares10@gmail.com>
2024-08-02 14:45:15 +0100

    feat: create header in home page

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,10 +1,30 @@
+import { ButtonLink } from "@/components/ui/button-link";
+import { Typography } from "@/components/ui/typography";
 import { useTranslations } from "next-intl";
+import { Github, LucideGithub, LucideLinkedin } from "lucide-react";
 
 export default function Home() {
   const t = useTranslations("Index");
   return (
-    <main>
-      <h1>{t("headerTitle")}</h1>
-    </main>
+    <section>
+      <header className="py-[10rem]">
+        <Typography
+          as="h1"
+          content={t("header.title")}
+          className="font-bold text-6xl w-[38rem]"
+        />
+
+        <div className="space-x-4 mt-2">
+          <ButtonLink
+            link="https/linkedin.com/teosoares10/"
+            content={<LucideLinkedin />}
+          />
+          <ButtonLink
+            link="https/github.com/teosoares10/"
+            content={<LucideGithub />}
+          />
+        </div>
+      </header>
+    </section>
   );
 }
